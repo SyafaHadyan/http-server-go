2a9fdeb792cd68b5d17e0f73ce2e1d5a6f219f00
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-04-26 18:47:17 +0700

    feat: read user agent handler

--- internal/app/bootstrap/bootstrap.go
+++ internal/app/bootstrap/bootstrap.go
@@ -4,6 +4,6 @@ import (
 	"github.com/SyafaHadyan/http-server-go/internal/app/handler"
 )
 
-func Start() (int, error) {
-	return handler.NewHandler()
+func Start() {
+	handler.NewHandler()
 }
--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -9,59 +9,75 @@ import (
 	"unicode/utf8"
 )
 
-type Handler struct{}
+type Handler struct {
+	listener net.Listener
+	conn     net.Conn
+}
 
-func handle(handler *Handler) (int, error) {
+func NewHandler() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	remote := c.RemoteAddr()
+		handler := Handler{
+			listener: l,
+			conn:     c,
+		}
+
+		_, _ = handle(&handler)
+
+		c.Close()
+	}
+}
+
+func handle(handler *Handler) (int, error) {
+	remote := handler.conn.RemoteAddr()
 
 	bytes := make([]byte, 1024)
-	status, err := c.Read(bytes)
+	status, err := handler.conn.Read(bytes)
 	if err != nil {
 		log.Println(remote)
 		return status, err
 	}
 
 	request := strings.Split(string(bytes), "\r\n")
-	requestLine := strings.Split(request[0], " ")
 
-	path := requestLine[1]
+	log.Println(strings.Join(request, ", "))
+
+	path := strings.Split(request[0], " ")[1]
 
 	if path == "/" {
-		status, err = handler.Root(c)
+		status, err = handler.Root(handler.conn)
 		if err != nil {
 			return status, err
 		}
 	} else if strings.HasPrefix(path, "/echo") {
-		status, err = handler.Echo(c, requestLine)
+		status, err = handler.Echo(handler.conn, request)
+		if err != nil {
+			return status, err
+		}
+	} else if strings.HasPrefix(path, "/user-agent") {
+		status, err = handler.UserAgent(handler.conn, request)
 		if err != nil {
 			return status, err
 		}
 	} else {
-		status, err := c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		status, err := handler.conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		if err != nil {
 			return status, err
 		}
 	}
 
 	return status, err
-}
-
-func NewHandler() (int, error) {
-	handler := Handler{}
-
-	return handle(&handler)
 }
 
 func (h *Handler) Root(c net.Conn) (int, error) {
@@ -73,12 +89,32 @@ func (h *Handler) Root(c net.Conn) (int, error) {
 	return status, err
 }
 
-func (h *Handler) Echo(c net.Conn, requestLine []string) (int, error) {
-	body := strings.ReplaceAll(requestLine[1], "/echo/", "")
-	format := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n", utf8.RuneCountInString(body))
-	echo := fmt.Sprintf(format+"%s", body)
+func (h *Handler) Echo(c net.Conn, request []string) (int, error) {
+	body := strings.Split(request[0], " ")[1]
+	body = strings.ReplaceAll(body, "/echo/", "")
+
+	echo := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+		utf8.RuneCountInString(body),
+		body)
 
 	status, err := c.Write([]byte(echo))
+	if err != nil {
+		return status, err
+	}
+
+	return status, err
+}
+
+func (h *Handler) UserAgent(c net.Conn, request []string) (int, error) {
+	body := strings.ReplaceAll(request[2], "User-Agent: ", "")
+
+	userAgent := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+		utf8.RuneCountInString(body),
+		body)
+
+	status, err := c.Write([]byte(userAgent))
 	if err != nil {
 		return status, err
 	}
