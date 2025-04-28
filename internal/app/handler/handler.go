fbd2b57a61cae2d74be946ef89fe56cf2f883dde
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-04-28 12:06:22 +0700

    fix: crlf

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -261,7 +261,7 @@ func (h *Handler) Echo(request []string) (int, error) {
 		)
 	} else {
 		echo = fmt.Sprintf(
-			"%sContent-Type: text/plain\r\nContent-Length: %d\r\n%s\r\n%s",
+			"%sContent-Type: text/plain\r\nContent-Length: %d\r\n%s%s",
 			httpStatus["ok"],
 			contentLength,
 			connection,
